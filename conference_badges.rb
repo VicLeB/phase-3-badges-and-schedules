ffc8c068f61e5f863f4fb6452dfb17ffd59c4db6
Victoria LeBel <vickileb@hotmail.com>
2022-07-25 09:19:03 -0700

    commit tree fa7e2d8542bd1d1ddf15076fe925487872351096

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_array = ["Arel", "Carol"]
+    name_array.map{|n| "Hello, my name is #{n}."}
+end
+
+def assign_rooms name_array = ["Arel", "Carol"]
+    name_array.map.with_index(1) {|name, i| "Hello, #{name}! You'll be assigned to room #{i}!"}
+end
+
+def printer names
+    batch_badge_creator(names).each {|n| puts n}
+
+
+    assign_rooms(names).each {|r| puts r}
+
+
+end
